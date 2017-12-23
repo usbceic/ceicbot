84f6b86d806366d9dd126cfd0545c42bd9f65c64
German Robayo Paz <germanrobayo33@gmail.com>
2017-12-23 17:59:59 -0400

    Anade middleware para privacidad, comando /miembros

--- index.js
+++ index.js
@@ -34,12 +34,47 @@ const allowedUsers = [
 
 const checkUsername = (userid) => allowedUsers.indexOf(userid) !== -1
 
-bot.start((ctx) => {
-  const {username, first_name: firstName} = ctx.from
-  return ctx.reply(checkUsername(username)
-    ? `Bienvenido ${firstName}
-Usa /help para ver la lista de comandos`
-    : `El bot es de uso privado de la JD-CEICUSB`)
+/**
+ * Middleware para que primero se verifique si el usuario es valido o no
+ */
+bot.use((ctx, next) => {
+  if (!ctx.from.username || !checkUsername(ctx.from.username)) {
+    ctx.reply('El bot es de uso privado de la JD-CEICUSB')
+    return
+  }
+  next()
 })
+
+/**
+ * Comando `/start`
+ * Solo saluda al usuario
+ */
+bot.start(ctx => {
+  return ctx.reply(`Bienvenido ${ctx.from.first_name}
+Usa /help para ver la lista de comandos`)
+})
+
+/**
+ * Comando `/help`
+ * Muestra los comandos disponibles del bot
+ */
+bot.command('help', ctx => {
+  const comandos = `La lista de comandos es:
+
+- /help Muestra esta lista de comandos
+- /miembros Lista los usernames de los miembros de la JD-CEICUSB`
+  ctx.reply(comandos)
+})
+
+/**
+ * Comando `/miembros`
+ * Lista los usernames de los miembros de la JD-CEICUSB
+ */
+bot.command('miembros', ctx => {
+  ctx.reply(`Los miembros de la JD-CEICUSB 2017-2018 son:
+
+${allowedUsers.map(user => `- @${user}`).join('\n')}`)
+})
+
 
 bot.startPolling()
