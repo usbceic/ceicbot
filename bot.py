29cdb0ef0d55d5d670dcd34cad75d067279c21ef
Luis Pino <lpino@darient.com>
2019-10-08 18:39:27 -0400

    Agregados imports base

---
+++ bot.py
@@ -0,0 +1,16 @@
+# File: bot.py
+#
+# Description: Archivo base para el desrrollo de un bot de telegram
+# del Centro de Estudiantes, ideado para realizar tareas como recibir
+# consultas sobre prodcutos del cuartico, enviar mensajes al ceic,etc...
+#
+# Autor: Informacion y Tecnologia CEIC-USB 2019
+#
+# Date: October - 2019
+
+###################### IMPORTS ##########################
+ 
+import telebot 
+from telebot import types  
+import time 
+ 
